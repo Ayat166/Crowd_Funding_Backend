5a954b8942d4581be6c65e4e8b4e89a8a6e98445
Ayat166 <ayatali1661@gmail.com>
2025-04-03 22:54:32 +0200

    Added donation logic

--- donations/urls.py
+++ donations/urls.py
@@ -1,4 +1,7 @@
-from django.contrib import admin
-from django.urls import path , include
+from django.urls import path
+from .views import DonationListCreateView, ProjectDonationListView
 
-urlpatterns = []
+urlpatterns = [
+    path('', DonationListCreateView.as_view(), name='donation-list-create'),
+    path('projects/<int:project_id>/', ProjectDonationListView.as_view(), name='project-donation-list'),
+]
