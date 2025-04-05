a2acf77ce0050bc39ed3a8d32d41310cde30c4d1
Ayat166 <ayatali1661@gmail.com>
2025-04-05 16:40:35 +0200

    Merged urls project file from ayat branch

--- projects/urls.py
+++ projects/urls.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from .views import RatingListCreateView
 
-urlpatterns = []
+urlpatterns = [
+    path('<int:project_id>/ratings/', RatingListCreateView.as_view(), name='rating-list-create'),
+]
