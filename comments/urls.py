652db972c0ac15441267ab2b57963d763f3c160c
Ayat166 <ayatali1661@gmail.com>
2025-04-05 16:30:28 +0200

    Merged urls comment file from ayat branch

--- comments/urls.py
+++ comments/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from .views import CommentListCreateView, CommentReplyCreateView, ReportListCreateView
 
-urlpatterns = []
+urlpatterns = [
+    path('projects/<int:project_id>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('comments/<int:comment_id>/replies/', CommentReplyCreateView.as_view(), name='comment-reply-create'),
+    path('reports/', ReportListCreateView.as_view(), name='report-list-create'),
+]
